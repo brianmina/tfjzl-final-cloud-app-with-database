a2a528554ac6a428a526e63b5c9a814e29976d3f
brian <brian.mina17@gmail.com>
2026-02-17 10:42:40 -0500

    name and bug addjusted

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -4,20 +4,15 @@ from django.conf.urls.static import static
 from . import views
 
 app_name = 'onlinecourse'
+
 urlpatterns = [
-    # route is a string contains a URL pattern
-    # view refers to the view function
-    # name the URL
-    path(route='', view=views.CourseListView.as_view(), name='index'),
+    path('', views.CourseListView.as_view(), name='index'),
     path('registration/', views.registration_request, name='registration'),
     path('login/', views.login_request, name='login'),
     path('logout/', views.logout_request, name='logout'),
-    # ex: /onlinecourse/5/
     path('<int:pk>/', views.CourseDetailView.as_view(), name='course_details'),
-    # ex: /enroll/5/
     path('<int:course_id>/enroll/', views.enroll, name='enroll'),
-    path('<int:course_id>/submit/', views.submit, name="submit"),
-    path('course/<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name="exam_result"),
+    path('<int:course_id>/submit/', views.submit, name='submit'),
+    path('course/<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name='exam_result'),
     path('<int:course_id>/', views.course_detail, name='course_detail'),
-    
- ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
